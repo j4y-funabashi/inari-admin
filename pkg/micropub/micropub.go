6f16f6f6ce6e1bc5e935828ff4cc58dce1e1cf67
Jay Robinson <me@j4y.co>
2019-03-29 12:20:06 +0000

    wip

--- pkg/micropub/micropub.go
+++ pkg/micropub/micropub.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/j4y_funabashi/inari-admin/pkg/mf2"
 	"github.com/j4y_funabashi/inari-admin/pkg/session"
 	"github.com/sirupsen/logrus"
 )
@@ -20,6 +21,7 @@ import (
 type MPClient interface {
 	UploadToMediaServer(uploadedFile UploadedFile, usess session.UserSession) (MediaEndpointResponse, error)
 	SendRequest(body url.Values, endpoint, bearerToken string) (MicropubEndpointResponse, error)
+	QueryPostList(micropubEndpoint, accessToken string) (mf2.PostList, error)
 }
 
 type GeoCoder interface {
@@ -55,11 +57,163 @@ type HttpResponse struct {
 }
 
 func (s *server) Routes(router *mux.Router) {
-	router.HandleFunc("/", s.HandleComposerForm())
 	router.HandleFunc("/composer", s.HandleComposerForm())
-	router.HandleFunc("/composer/addphoto", s.HandleAddPhotoForm())
 	router.HandleFunc("/composer/addlocation", s.HandleAddLocationForm())
 	router.HandleFunc("/submit", s.HandleSubmit())
+	router.HandleFunc("/composer/media/device", s.HandleAddPhotoForm())
+	router.HandleFunc("/composer/media/gallery", s.HandleQueryMedia())
+	router.HandleFunc("/queryposts", s.HandleQueryPosts())
+}
+
+func (s *server) HandleQueryMedia() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// fetch cookie
+		cookie, err := r.Cookie("sessionid")
+		if err != nil {
+			s.logger.WithError(err).Info("could not find sessionid cookie")
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		// fetch session
+		usess, err := s.SessionStore.FetchByID(cookie.Value)
+		if err != nil {
+			s.logger.WithError(err).Info("could not find session")
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+		s.logger.WithField("user", usess).Info("logged in user")
+
+		// query media endpoint
+		s.logger.WithField("media_endpoint", usess.MediaEndpoint).Info("Querying media endpoint")
+		req, err := http.NewRequest("GET", usess.MediaEndpoint+"?q=source", nil)
+		if err != nil {
+			s.logger.WithError(err).Error("failed to create GET request")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		req.Header.Set("Authorization", "Bearer "+usess.AccessToken)
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			s.logger.WithError(err).Error("failed to perform GET request")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		respBody := &bytes.Buffer{}
+		_, err = respBody.ReadFrom(resp.Body)
+		if err != nil {
+			s.logger.WithError(err).Error("failed to read GET request body")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		var mediaResponse mediaQueryListResponse
+		decoder := json.NewDecoder(respBody)
+		decoder.Decode(&mediaResponse)
+
+		s.logger.Infof("%+v", mediaResponse)
+
+		// render
+		t, err := template.ParseFiles(
+			"view/components.html",
+			"view/layout.html",
+			"view/medialist.html",
+		)
+		if err != nil {
+			s.logger.WithError(err).Error("failed to parse templat files")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		outBuf := new(bytes.Buffer)
+		v := struct {
+			PageTitle string
+			MediaList []mediaQueryListResponseItem
+		}{
+			PageTitle: "Choose a Video/Photo",
+			MediaList: mediaResponse.Items,
+		}
+		t.ExecuteTemplate(outBuf, "layout", v)
+
+		w.Header().Set("Content-type", "text/html; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(outBuf.Bytes())
+	}
+}
+
+type mediaQueryListResponse struct {
+	Items []mediaQueryListResponseItem `json:"items"`
+}
+type mediaQueryListResponseItem struct {
+	URL      string `json:"url"`
+	MimeType string `json:"mime_type"`
+}
+
+func (s *server) HandleQueryPosts() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// fetch cookie
+		cookie, err := r.Cookie("sessionid")
+		if err != nil {
+			s.logger.WithError(err).Info("could not find sessionid cookie")
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		// fetch session
+		usess, err := s.SessionStore.FetchByID(cookie.Value)
+		if err != nil {
+			s.logger.WithError(err).Info("could not find session")
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+		s.logger.WithField("user", usess).Info("logged in user")
+
+		// query post list
+		postList, err := s.client.QueryPostList(usess.MicropubEndpoint, usess.AccessToken)
+		if err != nil {
+			s.logger.WithError(err).Info("failed to query postlist")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		s.logger.Infof("%+v", postList)
+
+		// transform mf2 to jf2
+		var postListView []mf2.MicroFormatView
+		for _, postmf := range postList.Items {
+			postListView = append(postListView, postmf.ToView())
+		}
+
+		// render
+		t, err := template.ParseFiles(
+			"view/components.html",
+			"view/layout.html",
+			"view/postlist.html",
+		)
+		if err != nil {
+			s.logger.WithError(err).Error("failed to parse templat files")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		outBuf := new(bytes.Buffer)
+		v := struct {
+			PageTitle string
+			PostList  []mf2.MicroFormatView
+		}{
+			PageTitle: "LATEST POSTS",
+			PostList:  postListView,
+		}
+		t.ExecuteTemplate(outBuf, "layout", v)
+
+		w.Header().Set("Content-type", "text/html; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(outBuf.Bytes())
+	}
 }
 
 func (s *server) HandleSubmit() http.HandlerFunc {
@@ -86,7 +240,11 @@ func (s *server) HandleSubmit() http.HandlerFunc {
 	}
 }
 
-func (s *server) SubmitPost(sessionid, content, h string) HttpResponse {
+func (s *server) SubmitPost(
+	sessionid,
+	content,
+	h string,
+) HttpResponse {
 
 	// fetch session
 	usess, err := s.SessionStore.FetchByID(sessionid)
@@ -117,7 +275,14 @@ func (s *server) SubmitPost(sessionid, content, h string) HttpResponse {
 
 	s.logger.WithFields(logrus.Fields{"request": formData}).Info("built micropub request")
 
-	s.client.SendRequest(formData, usess.MicropubEndpoint, usess.AccessToken)
+	mpResponse, err := s.client.SendRequest(formData, usess.MicropubEndpoint, usess.AccessToken)
+	if err != nil {
+		s.logger.WithError(err).Error("failed to send MP request")
+		return HttpResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       err.Error(),
+		}
+	}
 
 	// TODO redirect with message if not successful
 	// TODO only clear session if successful
@@ -126,31 +291,11 @@ func (s *server) SubmitPost(sessionid, content, h string) HttpResponse {
 
 	// redirect
 	headers := map[string]string{
-		"Location": "/composer",
+		"Location": mpResponse.Location,
 	}
 	return HttpResponse{
-		StatusCode: http.StatusSeeOther,
+		StatusCode: mpResponse.StatusCode,
 		Headers:    headers,
-	}
-}
-
-func (s *server) HandleComposerForm() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		cookie, err := r.Cookie("sessionid")
-		if err != nil {
-			s.logger.Infof("redirecting, could not find sessionid cookie")
-			w.Header().Set("Location", "/login")
-			w.WriteHeader(http.StatusSeeOther)
-			return
-		}
-
-		response := s.ShowComposerForm(cookie.Value)
-		for k, v := range response.Headers {
-			w.Header().Set(k, v)
-		}
-		w.WriteHeader(response.StatusCode)
-		w.Write([]byte(response.Body))
 	}
 }
 
@@ -279,58 +424,6 @@ func parseFloat(f string) float64 {
 		return s
 	}
 	return 0
-}
-
-func (s *server) ShowComposerForm(sessionid string) HttpResponse {
-
-	// fetch session
-	usess, err := s.SessionStore.FetchByID(sessionid)
-	if err != nil {
-		return HttpResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:       err.Error(),
-		}
-	}
-	s.logger.WithFields(logrus.Fields{"user": usess}).Info("logged in user")
-
-	// render
-	t, err := template.ParseFiles(
-		"view/components.html",
-		"view/layout.html",
-		"view/composer.html",
-	)
-	if err != nil {
-		return HttpResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:       err.Error(),
-		}
-	}
-
-	w := new(bytes.Buffer)
-	v := struct {
-		PageTitle string
-		Photos    []session.MediaUpload
-		User      session.HCard
-		Published string
-		Location  string
-	}{
-		PageTitle: "Create Post",
-		Photos:    usess.ComposerData.Photos,
-		User:      usess.HCard,
-		Published: usess.ComposerData.Published,
-		Location:  usess.ComposerData.Location.ToGeoURL(),
-	}
-	t.ExecuteTemplate(w, "layout", v)
-
-	headers := map[string]string{
-		"Content-Type": "text/html; charset=UTF-8",
-	}
-
-	return HttpResponse{
-		StatusCode: http.StatusOK,
-		Body:       w.String(),
-		Headers:    headers,
-	}
 }
 
 type UploadedFile struct {
@@ -512,6 +605,111 @@ func (mpclient Client) UploadToMediaServer(uploadedFile UploadedFile, usess sess
 type MicropubEndpointResponse struct {
 	StatusCode int
 	Location   string
+}
+
+func (s *server) HandleComposerForm() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		cookie, err := r.Cookie("sessionid")
+		if err != nil {
+			s.logger.Infof("redirecting, could not find sessionid cookie")
+			w.Header().Set("Location", "/login")
+			w.WriteHeader(http.StatusSeeOther)
+			return
+		}
+
+		response := s.ShowComposerForm(cookie.Value)
+		for k, v := range response.Headers {
+			w.Header().Set(k, v)
+		}
+		w.WriteHeader(response.StatusCode)
+		w.Write([]byte(response.Body))
+	}
+}
+
+func (s *server) ShowComposerForm(sessionid string) HttpResponse {
+
+	// fetch session
+	usess, err := s.SessionStore.FetchByID(sessionid)
+	if err != nil {
+		return HttpResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       err.Error(),
+		}
+	}
+	s.logger.WithFields(logrus.Fields{"user": usess}).Info("logged in user")
+
+	// render
+	t, err := template.ParseFiles(
+		"view/components.html",
+		"view/layout.html",
+		"view/composer.html",
+	)
+	if err != nil {
+		return HttpResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       err.Error(),
+		}
+	}
+
+	w := new(bytes.Buffer)
+	v := struct {
+		PageTitle string
+		Photos    []session.MediaUpload
+		User      session.HCard
+		Published string
+		Location  string
+	}{
+		PageTitle: "Create Post",
+		Photos:    usess.ComposerData.Photos,
+		User:      usess.HCard,
+		Published: usess.ComposerData.Published,
+		Location:  usess.ComposerData.Location.ToGeoURL(),
+	}
+	t.ExecuteTemplate(w, "layout", v)
+
+	headers := map[string]string{
+		"Content-Type": "text/html; charset=UTF-8",
+	}
+	return HttpResponse{
+		StatusCode: http.StatusOK,
+		Body:       w.String(),
+		Headers:    headers,
+	}
+}
+
+func (mpclient Client) QueryPostList(micropubEndpoint, accessToken string) (mf2.PostList, error) {
+	var postList mf2.PostList
+
+	mpclient.logger.WithField("endpoint", micropubEndpoint).Info("Querying endpoint")
+
+	req, err := http.NewRequest("GET", micropubEndpoint+"?q=source", nil)
+	if err != nil {
+		return postList, err
+	}
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		mpclient.logger.WithError(err).Error("failed to perform GET request")
+		return postList, err
+	}
+	respBody := &bytes.Buffer{}
+	_, err = respBody.ReadFrom(resp.Body)
+	if err != nil {
+		mpclient.logger.WithError(err).Error("failed to read GET request body")
+		return postList, err
+	}
+	// parse response
+	decoder := json.NewDecoder(respBody)
+	err = decoder.Decode(&postList)
+	if err != nil {
+		mpclient.logger.WithError(err).Error("failed to decode json")
+		return postList, err
+	}
+	mpclient.logger.Infof("%+v", postList)
+
+	return postList, nil
 }
 
 func (mpclient Client) SendRequest(body url.Values, mpEndpoint, bearerToken string) (MicropubEndpointResponse, error) {
